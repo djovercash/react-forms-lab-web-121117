865deb8990914172ef307e45c72d77231f1aa434
github username <github email address>
2018-02-18 15:57:12 -0500

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,52 @@
 import React from "react";
+import PropTypes from 'prop-types'
 
 class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
+
+  handleUsername = event => {
+    let username = event.target.value
+    this.setState({
+      username: username,
+    })
+  }
+
+  handlePassword = event => {
+    let password = event.target.value
+    this.setState({
+      password: password,
+    })
+  }
+
+  handleSubmitLogin = event => {
+    event.preventDefault()
+    if (this.state.username === '' || this.state.password === '') {
+      return "Must have username or password"
+    } else {
+      this.props.onSubmit()
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmitLogin}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" name="username" value={this.state.username} onChange={this.handleUsername} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" name="password" value={this.state.password} onChange={this.handlePassword}/>
           </label>
         </div>
         <div>
@@ -30,4 +57,17 @@ class LoginForm extends React.Component {
   }
 }
 
+LoginForm.propTypes = {
+  onSubmit: PropTypes.func,
+}
+
 export default LoginForm;
+
+
+// Open the components/LoginForm.js file.
+// This component takes one prop, onSubmit, which is a function that gets called when the form is being submitted.
+// You'll find two inputs in this component: <input type="text"> and <input type="password">. Make this a controlled component by adding the necessary props to these inputs. Their values should be saved in the component's state.
+// An example of an input would look like: js <input id="test-username" type="text" name="username" value={this.state.username} onChange={this.handleInputChange} />
+// Remember that you can retrieve the input name and value of an event.target from the JS event.
+// Add the necessary event handler to the <form> element in order to call the onSubmit callback prop.
+// The onSubmit callback prop should only be called if both fields are filled in (with any value).
--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,14 +4,56 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      poem: '',
+      seen: 'visible'
+    };
   }
+
+  handlePoem = event => {
+    this.setState({
+      poem: event.target.value,
+    })
+
+    let poemArray = event.target.value.split('\n')
+
+    if (poemArray.length === 3) {
+      let line1 = poemArray[0].split(' ')
+      let line2 = poemArray[1].split(' ')
+      let line3 = poemArray[2].split(' ')
+
+
+      line1 = line1.filter(word => {
+        return word !== ''
+      })
+
+      line2 = line2.filter(word => {
+        return word !== ''
+      })
+
+      line3 = line3.filter(word => {
+        return word !== ''
+      })
+
+      if (line1.length === 5 && line2.length === 3 && line3.length === 5) {
+        this.setState({
+          seen: 'hidden'
+        })
+      } else {
+        console.log("FAIL")
+      }
+    } else {
+      console.log("FAIL")
+    }
+  }
+
+
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
-        <div id="poem-validation-error" style={{ color: "red" }}>
+        <textarea rows="3" cols="60" value={this.state.poem} onChange={this.handlePoem}/>
+        <div id="poem-validation-error" style={{ color: "red", visibility: this.state.seen }}>
           This poem is not written in the right structure!
         </div>
       </div>
@@ -20,3 +62,16 @@ class PoemWriter extends React.Component {
 }
 
 export default PoemWriter;
+
+
+//
+// Open the components/PoemWriter.js file.
+// You'll find one <textarea> in this component. Make this a controlled component. Recall that controlled components in React render dynamically based on some piece of props or state. Its value should be saved in the components state.
+// You'll also find an error element in the markup. This element should only be shown if the poem is not valid.
+// The rules for a valid poem structure are as follows:
+// The poem has three lines.
+// The first line has five words.
+// The second line has three words.
+// The third line has five words.
+// Be sure to account for users accidentally adding too many spaces! This means that ' I am a furry dog ' is not a valid first line of the poem.
+// Make sure the error message is still showing if the user deletes all of their content from the textarea box
